92629eb046da6b4599bf6c36120842f411cb80d7
shanghun1404 <shanghun1404@naver.com>
2026-09-06 04:13:36 +0900

    add half_adders

---
+++ project_1.srcs/sources_1/new/half_adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/09/05 05:34:04
+// Design Name: 
+// Module Name: half_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_adder(
+
+    );
+endmodule
